f44cb9813efa6cfa790648241636d5c4dd0b7943
karoo <karolinagonzalezv@ielacandelariamedellin.edu.co>
2026-03-20 21:14:32 +0000

    ejercicios js

---
+++ 20-03/Ejercicio1-1.js
@@ -0,0 +1,9 @@
+var nombre ="karolina";
+let edad =17;
+const ciudad ="medellin";
+console.log("nombre");
+console.log(nombre);
+console.log("la edad es :");
+console.log(edad);
+console.log("ciudad:",ciudad);
+console.log("Nombre :",nombre , "Edad:", edad ,"ciudad:", ciudad);
