ecf1f4e69917987b0afcb00b7d243205d63d87fe
karoo <karolinagonzalezv@ielacandelariamedellin.edu.co>
2026-03-20 21:22:44 +0000

    ejercicios js

---
+++ 20-03/ejercicio1-3.js
@@ -0,0 +1,6 @@
+let nota = 3;
+nota = 2;
+nota= 4; 
+console.log("Valor final con let:", nota);
+const metros = 9.8;
+console.log ("metros:",metros);
---
+++ 20-03/ejercicio1-4.js
@@ -0,0 +1,5 @@
+let precio = "350";
+let precioNumero = Number(precio) + 100;
+console.log(precioNumero); 
+let resultadoFinal = precioNumero.toString() + " pesos colombianos";
+console.log(resultadoFinal);
